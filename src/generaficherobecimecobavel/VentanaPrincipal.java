e23d32736f39427a9765ea072b54830cc5b09694
ivanw7 <ivanw7@ivanw7-PC>
2015-10-20 18:12:47 +0100

    intento1

--- src/generaficherobecimecobavel/VentanaPrincipal.java
+++ src/generaficherobecimecobavel/VentanaPrincipal.java
@@ -24,9 +24,6 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
-import com.toedter.calendar.JDateChooser;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import javax.swing.JComboBox;
@@ -40,56 +37,63 @@ public class VentanaPrincipal extends JFrame{
     JTable rejilla;
     JTextField txtFechaInicio;
     JTextField txtFechaFin;
-    JTextField txtNumCli;
-    static JTextField txtCliente;
+    JTextField txtCliente;
+    JButton botonExportar;
     JComboBox comboFormato;
     static DefaultTableModel modelo;
     JMenuBar barraMenuPrincipal;
     
     private void LanzaRellenaRejillaBD(){
         if (this.txtFechaInicio.getText().isEmpty() || this.txtFechaFin.getText().isEmpty()){
-            JOptionPane.showMessageDialog(null, "Elija un rango de fechas correcto");
+            JOptionPane.showMessageDialog(null, "Elija un rango de fechas correcto"); 
+        }else if (this.txtCliente.getText().isEmpty()){
+            JOptionPane.showMessageDialog(null, "Introduzca un código de cliente.");       
+                    
         }else
             switch (this.comboFormato.getSelectedIndex()){
                 case 0:
                     JOptionPane.showMessageDialog(null, "Elija un formato de archivo: Cofarte o BaVel");
                     break;
                 case 1:
-                    RellenaRejillaBD  rellenaBD1 = new RellenaRejillaBD ("SELECT AlbaranesVentasCab.cliente, AlbaranesVentasCab.RazonSocial, AlbaranesVentasCab.Factura, Facturas.fecha,AlbaranesVentasCab.Albaran, AlbaranesVentasCab.Fecha,IMPORTE DE ALBARAN SI NO HAY QUE CALCULARLO,Facturas.importe\n" +
+                    RellenaRejillaBD  rellenaBD1 = new RellenaRejillaBD ("SELECT AlbaranesVentasCab.cliente, AlbaranesVentasCab.RazonSocial, AlbaranesVentasCab.Factura, Facturas.fecha, AlbaranesVentasCab.Albaran, AlbaranesVentasCab.Fecha,AlbaranesVentasCab.Importe, Facturas.importe\n" +
                         "FROM dbo.AlbaranesVentasCab JOIN facturas on AlbaranesVentasCab.Factura=Facturas.factura JOIN AlbaranesVentasLin on AlbaranesVentasCab.Albaran=AlbaranesVentasLin.Albaran\n" +
                         "WHERE Facturas.Fecha BETWEEN '"+txtFechaInicio.getText()+"' AND '"+txtFechaFin.getText()+"' AND AlbaranesVentasCab.CLIENTE LIKE '"+txtCliente.getText()+"' AND AlbaranesVentasLin.Articulo<>'NO' \n" +
                         "ORDER BY AlbaranesVentasCab.ALBARAN desc");
                         //Nos evitamos cambios en los datos de consulta para que no haya diferencia con los ficheros volcados si se cambiaran dichos datos
                         this.comboFormato.setEnabled(false);
-                        /*this.datochooser deshabilitado
-                        this.dCFin.setEnabled(false);
-                        hacer lo contrario luego en el limpia rejilla*/
+                        bloqueaCampos();
+
+                        
                     break;
                 case 2:
-                    RellenaRejillaBD  rellenaBD2 = new RellenaRejillaBD ("SELECT AlbaranesVentasCab.cliente, AlbaranesVentasCab.RazonSocial, AlbaranesVentasCab.Factura, Facturas.fecha,AlbaranesVentasCab.Albaran, AlbaranesVentasCab.Fecha,IMPORTE DE ALBARAN SI NO HAY QUE CALCULARLO,Facturas.importe\n" +
+                    RellenaRejillaBD  rellenaBD2 = new RellenaRejillaBD ("SELECT AlbaranesVentasCab.cliente, AlbaranesVentasCab.RazonSocial, AlbaranesVentasCab.Factura, Facturas.fecha,AlbaranesVentasCab.Albaran, AlbaranesVentasCab.Fecha, AlbaranesVentasCab.Importe, Facturas.importe\n" +
                         "FROM dbo.AlbaranesVentasCab JOIN facturas on AlbaranesVentasCab.Factura=Facturas.factura JOIN AlbaranesVentasLin on AlbaranesVentasCab.Albaran=AlbaranesVentasLin.Albaran\n" +
                         "WHERE Facturas.Fecha BETWEEN '"+txtFechaInicio.getText()+"' AND '"+txtFechaFin.getText()+"' AND AlbaranesVentasCab.CLIENTE LIKE '"+txtCliente.getText()+"' AND AlbaranesVentasLin.Articulo<>'NO' \n" +
                         "ORDER BY AlbaranesVentasCab.ALBARAN desc");
                         //Nos evitamos cambios en los datos de consulta para que no haya diferencia con los ficheros volcados si se cambiaran dichos datos
-                        this.comboFormato.setEnabled(false);
-                        /*this.datochooser deshabilitado
-                        this.dCFin.setEnabled(false);
-                        hacer lo contrario luego en el limpia rejilla*/
+                        bloqueaCampos();
                     break;
             }
+            
 
     }
+     
+    //Nos evitamos cambios en los datos de consulta para que no haya diferencia con los ficheros volcados si se cambiaran dichos datos
+    private void bloqueaCampos(){
+        this.comboFormato.setEnabled(false);
+        this.txtFechaInicio.setEditable(false);
+        this.txtFechaFin.setEditable(false);
+        this.txtCliente.setEnabled(false);
+        this.botonExportar.setEnabled(true);//aquí permitimos exportar datos, una vez comprobados todos los campos y devulta la consulta previa ok.
+    }
     
- 
+    
     private void LanzaConsulta(){
         
         if (this.txtFechaInicio.getText().isEmpty() || this.txtFechaFin.getText().isEmpty()){
             JOptionPane.showMessageDialog(null, "Elija un rango de fechas correcto");
         }else
             switch (this.comboFormato.getSelectedIndex()){
-                case 0:
-                    JOptionPane.showMessageDialog(null, "Elija un formato de archivo: Cofarte o BaVel");
-                    break;
                 case 1:
                     String cadenaAPasar1=("SELECT Transporte, Albaran, Total=(ImporteBase1), Total2=(ImporteBase2), PjeIva1, PjeIva2, Importe, Fecha\n" +
                                             "FROM dbo.AlbaranesVentasCab\n" +
@@ -99,23 +103,22 @@ public class VentanaPrincipal extends JFrame{
                     FicherosCofarte  FCofarte = new FicherosCofarte (cadenaAPasar1);
                     break;
                 case 2:
-                    String cadenaAPasar2=("SELECT ';','406-', AlbaranesVentasCab.Factura, ';;',Facturas.fecha, ';','EUR',';' ,AlbaranesVentasLin.Articulo\n" +
-                        ",';', AlbaranesVentasLin.Descripcion, ';', 'Unidades', ';', AlbaranesVentasLin.Cantidad, ';', AlbaranesVentasLin.Precio\n" +
-                        ", ';', AlbaranesVentasLin.PjeDto, ';;', AlbaranesVentasLin.PjeIva, ';', 'IGIC', ';', (AlbaranesVentasLin.Importe*(AlbaranesVentasLin.PjeIva/100)) AS RED1 \n" +
-                        ",';', (((AlbaranesVentasLin.Cantidad*AlbaranesVentasLin.Precio)-(AlbaranesVentasLin.Precio*(AlbaranesVentasLin.PjeDto/100)))) AS RED2, ';'\n" +
-                        ",'406-',AlbaranesVentasCab.Albaran, ';', AlbaranesVentasCab.Fecha,';;'\n" +
-                        ", (Facturas.ImporteIva1+Facturas.ImporteIva2+Facturas.ImporteIva3+Facturas.ImporteIva4) AS RED3,';'"
-                        + ", (Facturas.ImporteBase1+Facturas.ImporteBase2+Facturas.ImporteBase3+Facturas.ImporteBase4),';'\n" +
+                    String cadenaAPasar2;
+                    cadenaAPasar2=("SELECT ';','406-', AlbaranesVentasCab.Factura, ';;',Facturas.fecha, ';','EUR',';' ,AlbaranesVentasLin.Articulo\n" +//9
+                        ",';', AlbaranesVentasLin.Descripcion, ';', 'Unidades', ';', AlbaranesVentasLin.Cantidad, ';', AlbaranesVentasLin.Precio\n" +//17
+                        ", ';', AlbaranesVentasLin.PjeDto, ';;', AlbaranesVentasLin.PjeIva, ';', 'IGIC', ';', (AlbaranesVentasLin.Importe*(AlbaranesVentasLin.PjeIva/100)) AS IMPIGICLIN \n" +//25
+                        ",';', (((AlbaranesVentasLin.Cantidad*AlbaranesVentasLin.Precio)-(AlbaranesVentasLin.Precio*(AlbaranesVentasLin.PjeDto/100)))) AS IMPLIN, ';'\n" +//28
+                        ",'406-',AlbaranesVentasCab.Albaran, ';', AlbaranesVentasCab.Fecha,';;'\n" +//33
+                        ", (Facturas.ImporteIva1+Facturas.ImporteIva2+Facturas.ImporteIva3+Facturas.ImporteIva4) AS IMPIGICTOTFAC,';'"//35
+                        + ", (Facturas.ImporteBase1+Facturas.ImporteBase2+Facturas.ImporteBase3+Facturas.ImporteBase4) AS BASIMP,';'\n" +//37
                         "FROM dbo.AlbaranesVentasCab\n" +
                         "JOIN facturas on AlbaranesVentasCab.Factura=Facturas.factura\n" +
                         "JOIN AlbaranesVentasLin on AlbaranesVentasCab.Albaran=AlbaranesVentasLin.Albaran\n" +
                         "WHERE Facturas.Fecha BETWEEN '"+txtFechaInicio.getText()+"' AND '"+txtFechaFin.getText()+"' AND AlbaranesVentasCab.CLIENTE LIKE '"+txtCliente.getText()+"' AND AlbaranesVentasLin.Articulo<>'NO'\n" +
                         "ORDER BY AlbaranesVentasCab.ALBARAN desc;");
-                    
-                    FicherosBavel  FBavel = new FicherosBavel (cadenaAPasar2);
-                    
-                    //System.out.println(cadenaAPasar);
-                    
+ 
+                    FicherosBavel  FBavel = new FicherosBavel (cadenaAPasar2, this.txtCliente.getText());
+
                     break;
             }
     }
@@ -130,9 +133,11 @@ public class VentanaPrincipal extends JFrame{
             JOptionPane.showMessageDialog(null, "Error al limpiar la tabla."+e);
         }
         this.comboFormato.setEnabled(true);
-        this.comboFormato.setSelectedIndex(0);
-        this.txtFechaInicio.setText("");
-        this.txtFechaFin.setText("");
+        this.comboFormato.setEnabled(true);
+        this.txtFechaInicio.setEditable(true);
+        this.txtFechaFin.setEditable(true);
+        this.txtCliente.setEnabled(true);
+        this.botonExportar.setEnabled(false);
     }
     private void haceNada(){
         
@@ -223,13 +228,14 @@ public class VentanaPrincipal extends JFrame{
 
         
         this.txtFechaInicio = new JTextField(10);
-        txtFechaInicio.setEditable(false);
+        //txtFechaInicio.setEditable(false);
         constraints.gridx = 1;
         constraints.gridy = 1;
         constraints.gridwidth=1;
         constraints.anchor = GridBagConstraints.WEST; //Situar el botón en la parte superior izq (anchor = situarlo sin estirarlo)
         this.getContentPane().add(txtFechaInicio,constraints);
         
+        /*
         final JDateChooser dCInicio = new JDateChooser();
         dCInicio.add(txtFechaInicio);
         //dCInicio.setDate();
@@ -243,7 +249,7 @@ public class VentanaPrincipal extends JFrame{
                 txtFechaInicio.setText(sdf.format(dCInicio.getDate())); //La metemos en el txtField.
             }
         }
-    });
+    });*/
 
         JLabel lblFechaFin = new JLabel("Fecha final:");
         constraints.gridx = 2;
@@ -253,14 +259,14 @@ public class VentanaPrincipal extends JFrame{
         this.getContentPane().add(lblFechaFin,constraints);
         
         this.txtFechaFin = new JTextField(10);
-        txtFechaFin.setEditable(false);
+        //txtFechaFin.setEditable(false);
         constraints.gridx = 2;
         constraints.gridy = 1;
         constraints.gridwidth=1;
         constraints.anchor = GridBagConstraints.WEST; //Situar el botón en la parte superior izq (anchor = situarlo sin estirarlo)
         this.getContentPane().add(txtFechaFin,constraints);
         
-        
+        /*
         final JDateChooser dCFin = new JDateChooser();
         dCFin.add(txtFechaFin);
         this.getContentPane().add(dCFin,constraints);
@@ -273,7 +279,7 @@ public class VentanaPrincipal extends JFrame{
                 txtFechaFin.setText(sdf.format(dCFin.getDate())); //La metemos en el txtField.    
             }
         }
-    });
+    });*/
         
         
         JLabel lblCLiente = new JLabel("Cliente:");
@@ -284,7 +290,6 @@ public class VentanaPrincipal extends JFrame{
         this.getContentPane().add(lblCLiente,constraints);
         
         this.txtCliente = new JTextField(10);
-        txtFechaFin.setEditable(false);
         constraints.gridx = 3;
         constraints.gridy = 1;
         constraints.gridwidth=1;
@@ -299,8 +304,8 @@ public class VentanaPrincipal extends JFrame{
         this.getContentPane().add(panelScroll, constraints); // añadimos un texto
         
         
-        JButton botonLimpiar = new JButton ("Limpar rejilla");
-        constraints.gridx = 0;
+        JButton botonLimpiar = new JButton ("Nueva consulta");
+        constraints.gridx = 5;
         constraints.gridy = 3;
         constraints.anchor = GridBagConstraints.EAST; //Situar el botón en la parte superior izq (anchor = situarlo sin estirarlo)
         ActionListener actionListenerParaLimpiar;       // creamos un actionlistener para salir (lo guardamos en una variable 
@@ -334,8 +339,9 @@ public class VentanaPrincipal extends JFrame{
         this.getContentPane().add(botonConsultar,constraints);
       
         
-        JButton botonExportar = new JButton ("Exportar");
-        constraints.gridx = 0;
+        botonExportar = new JButton ("Exportar");
+        this.botonExportar.setEnabled(false);
+        constraints.gridx = 3;
         constraints.gridy = 3;
         constraints.anchor = GridBagConstraints.CENTER; //Situar el botón en la parte superior izq (anchor = situarlo sin estirarlo)
         ActionListener actionListenerParaExportar;       // creamos un actionlistener para salir (lo guardamos en una variable 
